39f30d0d8207d85d25f82dc7dbcb55d460076335
LuckySingh21 <128302021+LuckySingh21@users.noreply.github.com>
2023-05-22 18:05:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let newh1 = document.createElement("h1");
+new.innerText = 'Width: ${window.innerWidth} and Height: ${window.innerHeight}';
+document.append(newh1);
+
+window.onresize = function(){
+	newh1.innerText = 'Width: ${window.innerWidth} and Height: ${window.innerHeight}';
+}
